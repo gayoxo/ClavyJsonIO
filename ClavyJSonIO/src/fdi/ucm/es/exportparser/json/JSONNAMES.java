5a1594d01d3c251b9b950081ba56c79ce9bbad66
Joaquin Gayoso-Cabada <jgayoso@ucm.es>
2018-06-28 16:06:45 +0200

    halaaaa

---
+++ ClavyJSonIO/src/fdi/ucm/es/exportparser/json/JSONNAMES.java
@@ -0,0 +1,27 @@
+package fdi.ucm.es.exportparser.json;
+
+public class JSONNAMES {
+
+	public static final String COLLECTION_ID = "id";
+	public static final String COLLECTION_NAME = "name";
+	public static final String COLLECTION_DESC = "description";
+	public static final String COLLECTION_RESOURCES = "resources";
+	public static final String COLLECTION_GRAMMARS = "grammars";
+	
+	public static final String RESOURCES_ID = "id";
+	public static final String RESOURCES_PATH = "path";
+	
+	public static final String GRAMMARS_ID = "id";
+	public static final String GRAMMARS_NAME = "nombre";
+	public static final String GRAMMARS_DESC = "description";
+	public static final String GRAMMARS_VIEW = "view";
+	public static final String GRAMMARS_SONS = "sons";
+	
+	public static final String GRAMMARS_VIEW_ID = "id";
+	public static final String GRAMMARS_VIEW_NAME = "name";
+	public static final String GRAMMARS_VIEW_DEF = "default";
+	public static final String GRAMMARS_VIEW_VIEW = "view";
+	
+	
+
+}
--- ClavyJSonIO/src/fdi/ucm/es/exportparser/json/SaveCollectionJSON.java
+++ ClavyJSonIO/src/fdi/ucm/es/exportparser/json/SaveCollectionJSON.java
@@ -4,14 +4,32 @@
 package fdi.ucm.es.exportparser.json;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 import fdi.ucm.server.modelComplete.ImportExportPair;
 import fdi.ucm.server.modelComplete.CompleteImportRuntimeException;
 import fdi.ucm.server.modelComplete.SaveCollection;
 import fdi.ucm.server.modelComplete.collection.CompleteCollection;
 import fdi.ucm.server.modelComplete.collection.CompleteLogAndUpdates;
+import fdi.ucm.server.modelComplete.collection.document.CompleteFile;
+import fdi.ucm.server.modelComplete.collection.grammar.CompleteGrammar;
+import fdi.ucm.server.modelComplete.collection.grammar.CompleteOperationalValueType;
 
 /**
  * Clase que impementa el plugin de oda para Localhost
@@ -25,7 +43,6 @@ public class SaveCollectionJSON extends SaveCollection {
 	private String Path;
 	private String FileIO;
 	private String SOURCE_FOLDER = ""; // SourceFolder path
-	private static final Pattern regexAmbito = Pattern.compile("^[0-9]+(,[0-9]+)*$");
 
 	
 	/**
@@ -51,6 +68,13 @@ public class SaveCollectionJSON extends SaveCollection {
 			Dir.mkdirs();
 			FileIO = Path+System.currentTimeMillis()+".json";
 			
+			String Salida = processCollection(Salvar);
+			
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			JsonParser jp = new JsonParser();
+			JsonElement je = jp.parse(Salida);
+			String prettyJsonString = gson.toJson(je);
+			System.out.println(prettyJsonString);
 //			HTMLprocess oda= new HTMLprocess(ListaDeDocumentos,Salvar,SOURCE_FOLDER,CL,TextoIn);
 //			
 //			
@@ -59,14 +83,84 @@ public class SaveCollectionJSON extends SaveCollection {
 
 				CL.getLogLines().add("Descarga el zip");
 
-
+				FileWriter fw=new FileWriter(FileIO);
+				fw.write(prettyJsonString);
+				fw.close(); 
 			return CL;
 
 
 		} catch (CompleteImportRuntimeException e) {
-			System.err.println("Exception HTML " +e.getGENERIC_ERROR());
+			System.err.println("Exception JSON " +e.getGENERIC_ERROR());
 			e.printStackTrace();
 			throw e;
+		} catch (IOException e) {
+			System.err.println("Exception HTML " +e.getMessage());
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		
+	}
+
+	private String processCollection(CompleteCollection salvar) {
+		JSONObject Col=new JSONObject();
+		try {
+			Col.put(JSONNAMES.COLLECTION_ID, salvar.getClavilenoid());
+			Col.put(JSONNAMES.COLLECTION_NAME, salvar.getName());
+			Col.put(JSONNAMES.COLLECTION_DESC, salvar.getDescription());
+			insertSections(Col,salvar);
+			insertGrammars(Col,salvar);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return Col.toString();
+	}
+
+	private void insertGrammars(JSONObject col, CompleteCollection salvar) throws JSONException {
+		JSONArray Gramm=new JSONArray();
+		col.put(JSONNAMES.COLLECTION_GRAMMARS, Gramm);
+		for (int i = 0; i < salvar.getMetamodelGrammar().size(); i++) {
+			CompleteGrammar grammar = salvar.getMetamodelGrammar().get(i);
+			JSONObject GrammarJ=new JSONObject();
+			GrammarJ.put(JSONNAMES.GRAMMARS_ID, grammar.getClavilenoid());
+			GrammarJ.put(JSONNAMES.GRAMMARS_NAME,grammar.getNombre());
+			GrammarJ.put(JSONNAMES.GRAMMARS_DESC,grammar.getDescription());
+			insertgrammarView(GrammarJ,grammar);
+			insertgrammarSons(GrammarJ,grammar);
+			Gramm.put(GrammarJ);
+		}
+		
+	}
+
+	private void insertgrammarSons(JSONObject grammarJ, CompleteGrammar grammar) throws JSONException {
+		JSONArray Sons=new JSONArray();
+		grammarJ.put(JSONNAMES.GRAMMARS_SONS, Sons);
+		//TODO
+	}
+
+	private void insertgrammarView(JSONObject grammarJ, CompleteGrammar grammar) throws JSONException {
+		JSONArray views=new JSONArray();
+		grammarJ.put(JSONNAMES.GRAMMARS_VIEW, views);
+		for (int i = 0; i < grammar.getViews().size(); i++) {
+			CompleteOperationalValueType GOv = grammar.getViews().get(i);
+			JSONObject Operational=new JSONObject();
+			Operational.put(JSONNAMES.GRAMMARS_VIEW_ID, GOv.getClavilenoid());
+			Operational.put(JSONNAMES.GRAMMARS_VIEW_NAME,GOv.getName());
+			Operational.put(JSONNAMES.GRAMMARS_VIEW_DEF,GOv.getDefault());
+			Operational.put(JSONNAMES.GRAMMARS_VIEW_VIEW,GOv.getView());
+			views.put(Operational);
+		}
+	}
+
+	private void insertSections(JSONObject col, CompleteCollection salvar) throws JSONException {
+		JSONArray Sec=new JSONArray();
+		col.put(JSONNAMES.COLLECTION_RESOURCES, Sec);
+		for (int i = 0; i < salvar.getSectionValues().size(); i++) {
+			CompleteFile file = salvar.getSectionValues().get(i);
+			JSONObject File=new JSONObject();
+			File.put(JSONNAMES.RESOURCES_ID, file.getClavilenoid());
+			File.put(JSONNAMES.RESOURCES_PATH,file.getPath());
+			Sec.put(File);
 		}
 		
 	}
@@ -154,16 +248,39 @@ public class SaveCollectionJSON extends SaveCollection {
  
 	
 	public static void main(String[] args) {
-		System.out.println(testList("6658,6658,6658,asd"));
-		System.out.println(testList("6658"));
+		SaveCollectionJSON SV=new SaveCollectionJSON();
+		CompleteCollection CC=CreateCollectionBase();
+		SV.processCollecccion(CC, "");
 	}
 
-	private static boolean testList(String number) {
-		if (number==null||number.isEmpty())
-			return true;
-		 Matcher matcher = regexAmbito.matcher(number);
-		return matcher.matches();
+	private static CompleteCollection CreateCollectionBase() {
+		CompleteCollection C=new CompleteCollection("nombre coleccion", "descripcion");
+		C.setClavilenoid(1l);
+		int n = (new Random()).nextInt(20);
+		for (int i = 0; i < n; i++) 
+			C.getSectionValues().add(new CompleteFile(new Long(i),"File"+ i+"path", C));
+		
+		List<CompleteFile> Files=new LinkedList<>(C.getSectionValues());
+		
+		int n2 = (new Random()).nextInt(20);
+		for (int i = 0; i < n2; i++) 
+			{	CompleteGrammar GG=new CompleteGrammar(new Long(i), "Gram"+ i+"name","Gram"+ i+"desc",C);
+				
+
+			int n3 = (new Random()).nextInt(10);
+			for (int j = 0; j < n3; j++) 
+				{	CompleteOperationalValueType OP=new CompleteOperationalValueType(new Long(j),"View"+ j+"name","View"+ j+"desc","View"+j+"padre");
+					GG.getViews().add(OP);
+				}
+				
+			
+				C.getMetamodelGrammar().add(GG);
+			}
+		
+		return C;
 	}
+
+
 	
 	
 	
